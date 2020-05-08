02bdcb85f23499cc4da44b6a1017d17a56e00319
Anita John <anitaj1@learn.cssd.ab.ca>
2020-05-07 18:11:38 -0600

    Made a Widget, and 3 Containers

--- lib/main.dart
+++ lib/main.dart
@@ -10,8 +10,31 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.teal,
-        body: Container(),
+        backgroundColor: Colors.black,
+        body: SafeArea(
+          child: Column(
+            children: <Widget>[
+              Container(
+                height: 100.0,
+                width: 100.0,
+                color: Colors.white,
+                child: Text('Container 1'),
+              ),
+              Container(
+                width: 100.00,
+                height:100.0,
+                color:Colors.blue,
+                child: Text('Container 2'),
+              ),
+              Container(
+                width: 100.00,
+                height:100.0,
+                color:Colors.red,
+                child: Text('Container 2'),
+              ),
+            ],
+          ) ,
+        ),
       ),
     );
   }
